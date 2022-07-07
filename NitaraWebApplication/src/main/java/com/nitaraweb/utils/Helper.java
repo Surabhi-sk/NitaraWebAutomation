bbbf066f5ebf3313059ae10aa1b57727c22498dc
Surabhi-sk <77966422+Surabhi-sk@users.noreply.github.com>
2022-07-07 13:01:10 +0530

    Create Helper.java

---
+++ NitaraWebApplication/src/main/java/com/nitaraweb/utils/Helper.java
@@ -0,0 +1,25 @@
+package com.nitaraweb.utils;
+
+
+public class Helper{
+  
+  	public String generateNo(int digits) {
+		Faker faker = new Faker();
+		String number = faker.number().digits(digits);
+		System.out.println(number);
+		return number;
+
+	}
+  
+  public JSONObject getMonthandYear(int months) {
+		JSONObject obj = new JSONObject();
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MONTH,-months);
+		obj.put("Year", new SimpleDateFormat("yyyy").format(cal.getTime()));
+		obj.put("Month", new SimpleDateFormat("MM").format(cal.getTime()));
+		obj.put("Date", new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime()));
+		return obj;
+	}
+	
+  
+}
